46f1ac8984dfc720f2515ad54f9f649d477b990c
RenatoBorgess <118858842+RenatoBorgess@users.noreply.github.com>
2023-05-08 16:04:11 -0300

    fix: changes classes names following naming conventions.

--- src/main/java/com/cms/claimmanagement/insuranceCompany/controller/CompanyController.java
+++ src/main/java/com/cms/claimmanagement/insuranceCompany/controller/CompanyController.java
@@ -3,6 +3,6 @@ package com.cms.claimmanagement.insuranceCompany.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class companyController {
+public class CompanyController {
 
 }
