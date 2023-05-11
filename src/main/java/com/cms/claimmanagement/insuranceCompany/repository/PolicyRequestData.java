5b1af82dc2c62e749051558d0a9adc3eb4675f7d
RenatoBorgess <118858842+RenatoBorgess@users.noreply.github.com>
2023-05-11 11:01:27 -0300

    feat: mapping entities relationships.

--- src/main/java/com/cms/claimmanagement/insuranceCompany/repository/PolicyRequestData.java
+++ src/main/java/com/cms/claimmanagement/insuranceCompany/repository/PolicyRequestData.java
@@ -1,12 +1,10 @@
 package com.cms.claimmanagement.insuranceCompany.repository;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 import java.time.LocalDate;
 
-public record PolicyData(
+public record PolicyRequestData(
 
         String InsuredFirstName,
         String InsuredLastName,
--- src/main/java/com/cms/claimmanagement/surveyor/repository/Surveyor.java
+++ src/main/java/com/cms/claimmanagement/surveyor/repository/Surveyor.java
@@ -1,18 +1,63 @@
 package com.cms.claimmanagement.surveyor.repository;
 
+import com.cms.claimmanagement.insuranceCompany.repository.ClaimDetails;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
+
+import java.util.List;
 
 @Entity
 public class Surveyor {
 
+    @Column(name = "surveyor_id")
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-
-    private Long surveyorId;
-
+    private Long id;
+    @OneToMany(mappedBy = "surveyor")
+    private List<ClaimDetails> claimDetails;
     String firstName;
     String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<ClaimDetails> getClaimDetails() {
+        return claimDetails;
+    }
+
+    public void setClaimDetails(List<ClaimDetails> claimDetails) {
+        this.claimDetails = claimDetails;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getEstimateLimit() {
+        return estimateLimit;
+    }
+
+    public void setEstimateLimit(Long estimateLimit) {
+        this.estimateLimit = estimateLimit;
+    }
+
     @Min(value = 0L, message = "The value must be positive")
     Long estimateLimit;
 }
