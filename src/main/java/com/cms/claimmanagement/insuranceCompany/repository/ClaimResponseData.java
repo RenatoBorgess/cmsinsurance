79873fca45c20993b097159e691c684dfdd55d88
RenatoBorgess <118858842+RenatoBorgess@users.noreply.github.com>
2023-05-16 11:04:28 -0300

    feat: creating a new claim files

--- src/main/java/com/cms/claimmanagement/insuranceCompany/repository/ClaimResponseData.java
+++ src/main/java/com/cms/claimmanagement/insuranceCompany/repository/ClaimResponseData.java
@@ -1,4 +1,33 @@
 package com.cms.claimmanagement.insuranceCompany.repository;
 
-public record ClaimResponseData() {
+import com.cms.claimmanagement.surveyor.repository.Surveyor;
+
+import java.time.LocalDate;
+
+public record ClaimResponseData(PolicyEntity policy,
+                                Long estimatedLoss,
+                                LocalDate dateOfAccident,
+                                boolean claimStatus,
+                                Surveyor surveyor,
+                                Long amtApprovedBySurveyor,
+                                boolean insuranceCompanyApproval,
+                                boolean withdrawClaim,
+                                Long surveyorFees) {
+     public ClaimResponseData(ClaimDetails claim){
+         this(
+                 claim.getPolicy(),
+                 claim.getEstimatedLoss(),
+                 claim.getDateOfAccident(),
+                 claim.isClaimStatus(),
+                 claim.getSurveyor(),
+                 claim.getAmtApprovedBySurveyor(),
+                 claim.isInsuranceCompanyApproval(),
+                 claim.isWithdrawClaim(),
+                 claim.getSurveyorFees());
+
+
+
+     }
+
+
 }
